4675cfcf928bb4c36cf28493735b8bbd7d1cff9c
marth141 <ncasados@live.com>
2022-07-23 17:17:35 -0600

    being classy with classes

--- apps/web/lib/web/live/twilio_live/index.ex
+++ apps/web/lib/web/live/twilio_live/index.ex
@@ -14,7 +14,7 @@ defmodule Web.TwilioLive.Index do
      |> assign(queue_token: queue_token)
      |> assign(
        :button_class,
-       "inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+       "inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
      )}
   end
 end
